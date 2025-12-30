4378ec15b0e28e38754166ae4ea3f3247e705cc4
Varun Bhardwaj <varunbhardwaj3416@gmail.com>
2025-12-30 13:06:42 +0530

    Pushing Q1.c

---
+++ array/Q1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <limits.h>
+
+int array[100000] ; 
+int main(){
+	int n ; 
+	printf("Enter the no of element's : \n") ; 
+	scanf("%d" , &n) ; 
+
+	int temp ; 
+	printf("Enter the elements : \n") ; 
+
+	// For secondMax and Max we use two counters 
+	int sM , M ; 
+	sM = M = INT_MIN ; 
+
+	for(int i = 0 ; i < n ; ++i){ 
+		scanf("%d" , &temp) ; 
+		array[i] = temp ; 
+		if (array[i] > M ){
+			sM = M ; 
+			M = array[i] ; 		
+ 		}
+		else if (array[i] > sM && array[i] != M) {
+			sM = array[i] ; 
+		}
+	}
+
+	if (sM == -1){ 
+		printf("-1 \n") ;
+	}
+	
+	else{
+	printf("%d \n" , sM) ; 
+	} 
+
+
+	return 0 ; 
+}
+
