c11eddcb4036f3c2de7d989f9d41771bfb82900b
Varun Bhardwaj <varunbhardwaj3416@gmail.com>
2026-01-07 11:33:24 +0530

    array : No of sub array whose sum less than or equal to k

---
+++ array/no_of_subarray_whose_sum_is_lessOrEqual_to_k.c
@@ -0,0 +1,61 @@
+/* Problem statement - Find the no of sub arrays whose sum is less than or equal to k 
+Approach - Sliding window technique is used . 
+*/
+
+#include <stdio.h> 
+
+int A[100000] ; 
+
+int noOfSubArray(int A[] , int n , int k ){
+    int l = 0 , sum = 0 , count = 0 ;
+
+    for(int r = 0 ; r < n ; ++r) {
+            sum += A[r] ; 
+            if (A[r] <= k ){
+                while(sum > k) {
+                    sum -= A[l] ; 
+                    l++ ; 
+                }
+                count += r - l + 1 ; 
+            }
+            else{
+                sum = 0 ;  
+                l = r + 1 ;    
+        }
+
+            
+    }
+
+    return count ; 
+
+}
+
+
+
+
+int main (){
+    int n ; 
+    printf("Enter the no of elements : \n") ; 
+    scanf("%d" , &n) ; 
+
+    int k , temp ; 
+    printf("Enter k : \n") ; 
+    scanf("%d" , &k) ; 
+
+    printf("Enter the elements : \n") ; 
+    for (int i = 0 ; i < n ; ++i) {
+            scanf("%d" , &temp) ; 
+            A[i] = temp ; 
+
+    }
+
+    int nSa = noOfSubArray(A , n , k) ; 
+
+
+
+    printf("noOfSubArrays are %d \n" ,nSa) ; 
+
+
+
+    return 0 ;
+}
